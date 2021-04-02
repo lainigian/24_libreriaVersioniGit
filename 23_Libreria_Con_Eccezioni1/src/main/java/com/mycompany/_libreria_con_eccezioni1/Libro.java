afb4126cd1b49b193a0aba36d3c80a1b868bad08
Gian Marco Laini <lainigian@yahoo.it>
2021-04-02 15:21:58 +0200

    CommentiJavadoc alla classe libro e alla classe Scaffale

---
+++ 23_Libreria_Con_Eccezioni1/src/main/java/com/mycompany/_libreria_con_eccezioni1/Libro.java
@@ -0,0 +1,165 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany._libreria_con_eccezioni1;
+
+/**
+ *La classe Libro rappresenta un libro. I suoi attributi sono:
+ * titolo
+ * autore
+ * numeroPagine
+ * costoPagina: indica il costo di una singola pagina del libro
+ * COSTO_FISSO: è una costante, indica la parte di costo fisso per la produzione di un libro
+ * il prezzo finale di un libroi sarà dato dalla somma fra il COSTO_FISSO  e numero di pagine moltiplicato per il costoPagina
+ * @author Laini Gian Marco
+ * @version 1.0
+ */
+
+public class Libro 
+{
+  //attibuti
+    private String titolo;
+    private String autore;
+    private int numeroPagine;
+    private static double costoPagina=0.05;
+    private final double COSTO_FISSO=5.5;
+  
+/**
+ * Costruttore della classe Libro. Consente di istanziare un nuovo libro
+ * @param titolo:titolo del libro
+ * @param autore:autore del libro
+ * @param numeroPagine: numero di pagine di cui è composto il libro   
+*/
+    public Libro(String titolo,String autore,int numeroPagine)
+    {
+        this.titolo=titolo;
+        this.autore=autore;
+        this.numeroPagine=numeroPagine;
+    }
+    
+  /**
+   * Costruttore di copia della classe Libro. Consente di istanziare un nuovo libro
+   * @param l: libro da cui verrà istanziato il nuovo libro. Il libro istanziato sarà una copia del libro l
+   */
+    public Libro(Libro l)
+    {
+        titolo=l.getTitolo();
+        autore=l.getAutore();
+        numeroPagine=l.getNumeroPagine();
+    }
+    
+    /**
+     * Costruttore di default della classe Libro. Consente di istanziare un nuovo libro con i seguenti 
+     * valori di default per gli attributi
+     * titolo: stringa vuota
+     * autore: stringa vuota 
+     * numeroPagine=0
+    */
+    public Libro()
+    {
+        titolo="";
+        autore="";
+        numeroPagine=0;
+    }
+    
+    /**
+     * Restituisce il titolo del libro
+     * @return titolo
+     */
+    public String getTitolo()
+    {
+        return titolo;
+    }
+    
+    /**
+     * Assegna il titolo al libro
+     * @param titolo 
+     */
+    public void setTitolo(String titolo)
+    {
+        this.titolo=titolo;
+    }
+    
+    /**
+     * restituisce l'autore del libro
+     * @return autore
+     */
+    public String getAutore()
+    {
+        return autore;   
+    }
+    
+    /**
+     * Assegna l'autore al libro
+     * @param autore 
+     */
+    public void setAutore(String autore)
+    {
+        this.autore=autore;
+    }
+    
+    /**
+     * Restituisce il numero di pagine di cui è composto il libro
+     * @return numeroPagine
+     */
+    public int getNumeroPagine()
+    {
+        return numeroPagine;
+    }
+    
+    /**
+     * Assegna il numero di pagine di cui è costituito il libro
+     * @param numeroPagine 
+     */
+    public void setNumeroPagine(int numeroPagine)
+    {
+        this.numeroPagine=numeroPagine;
+    }
+    
+    /**
+     * Calcola e restituisce il prezzo di vendita del libro
+     * @return prezzo
+     */
+    public double prezzo()
+    {
+        double p;
+        p=COSTO_FISSO+costoPagina*getNumeroPagine();
+        return p;
+    }
+    
+    /**
+     * Metodo statico
+     * Assegna il costo unitario per realizzare ciascuna pagina del libro
+     * 
+     * @param costo 
+     */
+    public static void setCostoPagina(double costo)
+    {
+        costoPagina=costo; 
+        
+    }
+    
+    /**
+     * Restituisce il costo unitario per realizzare ciascuna pagina del libro
+     * @return costoPagina
+     */
+    public static double getCostoPagina()
+    {
+        return costoPagina;
+    }
+    
+    /**
+     * Restituisce una stringa che rappresenta il libro
+     * @return stringa con titolo,autore e numero di pagine del libro
+     */
+    public String toString()
+    {
+        String s;
+        s=getTitolo()+";"+getAutore()+";"+getNumeroPagine()+" pagine";
+        return s;
+    }
+    
+}
+ 
---
+++ 23_Libreria_Con_Eccezioni1/src/main/java/com/mycompany/_libreria_con_eccezioni1/Scaffale.java
@@ -0,0 +1,453 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany._libreria_con_eccezioni1;
+
+import eccezioni.*;
+
+/**
+ *
+ * @author User
+ */
+public class Scaffale 
+{
+   private Mensola[] ripiani;
+   private static final int NUM_RIPIANI=5;
+   
+   public Scaffale()
+   {
+       ripiani=new Mensola[NUM_RIPIANI];
+       
+       for (int i=0;i<NUM_RIPIANI;i++)
+       {
+           ripiani[i]=new Mensola();
+       }
+       
+   }
+   
+   public Scaffale(Scaffale s) throws EccezionePosizioneNonValida, EccezionePosizioneNonVuota
+   {
+        ripiani=new Mensola[NUM_RIPIANI];
+        
+        Libro l;
+        for (int i=0;i<getNumRipiani();i++)
+        {
+            ripiani[i]=new Mensola();
+            for(int j=0;j<ripiani[i].getNumMaxVolumi();j++)
+            {
+                if (s.getLibro(i, j)!=null)
+                {
+                    l=s.getLibro(i, j);
+                    setLibro(l, i, j);
+                }
+            }
+        }
+        
+   }
+   
+   public Mensola getRipiano(int ripiano) throws EccezionePosizioneNonValida
+   {
+       Mensola m;
+       try
+       {
+            m=new Mensola(ripiani[ripiano]);
+            return m;
+       }
+       catch(ArrayIndexOutOfBoundsException e1)
+       {
+           throw new EccezionePosizioneNonValida(ripiano,0);
+       }
+  
+   }
+   
+  
+   
+   /*
+   Inserisce il libro nella "posizione" del “ripiano”. 
+    se il ripiano non è valido  --> return -3
+    se la posizione non è valida --> return -1 se è occupata --> return -2
+    se ok  return 0
+
+   
+   */
+   public void setLibro(Libro libro,int ripiano, int posizione) throws EccezionePosizioneNonValida, EccezionePosizioneNonVuota
+   {
+       int inserimentoOk;
+       try
+       {
+            inserimentoOk=ripiani[ripiano].setVolume(libro, posizione);
+            if (inserimentoOk==-1)
+                throw new EccezionePosizioneNonValida(ripiano, posizione);  //posizione non valida
+            else if (inserimentoOk==-2)
+                throw new EccezionePosizioneNonVuota(ripiano, posizione);   //posizione non vuota
+     
+       }
+       catch(ArrayIndexOutOfBoundsException e1)
+       {
+           throw new EccezionePosizioneNonValida(ripiano, posizione);   //ripiano non valido
+       }  
+   }
+   
+   /*
+   Restituisce il libro nella "posizione" del “ripiano”. 
+se il ripiano non è valido, la posizione non è valida o vuota --> return null
+se ok  ritorna l’oggetto libro
+   */
+    
+   public Libro getLibro(int ripiano, int posizione) throws EccezionePosizioneNonValida
+   {
+       try
+       {
+           return(ripiani[ripiano].getVolume(posizione));
+       }
+       catch(ArrayIndexOutOfBoundsException e1)
+       {
+           throw new EccezionePosizioneNonValida(ripiano, posizione);
+       }
+       
+       
+   }
+   
+   
+   /*
+   Libera la "posizione" nel “ripiano”. 
+se il ripiano non è valido  --> return -3
+se la posizione non è valida --> return -1 se già vuota --> return -2
+se ok  return 0
+
+   */
+   
+   public void rimuoviLibro(int ripiano, int posizione) throws EccezionePosizioneNonValida, EccezionePosizioneVuota
+   {
+       int rimozioneOk;
+       try
+       {
+           rimozioneOk=ripiani[ripiano].rimuoviVolume(posizione);
+           if (rimozioneOk==-1)
+                throw new EccezionePosizioneNonValida(ripiano, posizione);
+           else if (rimozioneOk==-2)
+               throw  new EccezionePosizioneVuota(ripiano, posizione);
+       }
+       catch(ArrayIndexOutOfBoundsException e1)
+       {
+           throw new EccezionePosizioneNonValida(ripiano, posizione);
+       }
+       
+   }
+   
+   public static int getNumRipiani()
+   {
+       return NUM_RIPIANI;
+   }
+   
+   public int getNumMaxLibri()
+   {
+       int numMaxLibri=0;
+       for(int i=0;i<getNumRipiani();i++)
+       {
+           numMaxLibri+=ripiani[i].getNumMaxVolumi();
+       }
+       return numMaxLibri;
+   }
+   
+   public int getNumLibri()
+   {
+       int numLibri=0;
+       for(int i=0;i<getNumRipiani();i++)
+       {
+           numLibri+=ripiani[i].getNumVolumi();
+       }
+       return numLibri;
+   }
+   
+   //se ripiano non valido --> return -1
+   public int getNumLibri(int ripiano) throws EccezionePosizioneNonValida
+   {
+       try
+       {
+           return ripiani[ripiano].getNumVolumi();
+       }
+       catch(ArrayIndexOutOfBoundsException e1)
+       {
+           throw new EccezionePosizioneNonValida(ripiano, 0);
+       }
+       
+       
+   }
+   
+   public int getNumMaxLibri(int ripiano) throws EccezionePosizioneNonValida
+   {
+       try
+       {
+           return ripiani[ripiano].getNumMaxVolumi();
+       }
+       catch(ArrayIndexOutOfBoundsException e1)
+       {
+           throw new EccezionePosizioneNonValida(ripiano, 0);
+       }
+       
+       
+   }
+   
+   public String[] elencoTitoliAutore(String autore) throws EccezionePosizioneNonValida
+   {
+       int numeroLibriAutore=0;
+       Libro libro;
+       
+       //conto il numero di libri dell'autore prensenti
+       for (int i=0;i<getNumRipiani();i++)
+       {
+           for (int j=0;j<ripiani[i].getNumMaxVolumi();j++)
+           {
+               if (getLibro(i, j)!=null)
+               {
+                   libro=getLibro(i, j);
+                   if (libro.getAutore().equalsIgnoreCase(autore))
+                       numeroLibriAutore++;
+               }
+           }
+       }
+       
+       // se nn ci sono libri di quell'autore, return null
+       if (numeroLibriAutore==0)
+           return null;
+       
+       String[] elencoTitoliAutore=new String[numeroLibriAutore];
+       
+       int posizioneTitolo=0;
+       
+       //Assegno ad ogni elemento dell'array il titolo del libro
+       for (int i=0;i<getNumRipiani();i++)
+       {
+           for (int j=0;j<ripiani[i].getNumMaxVolumi();j++)
+           {
+               if (getLibro(i, j)!=null)
+               {
+                   libro=getLibro(i, j);
+                   if (libro.getAutore().equalsIgnoreCase(autore))
+                   {
+                       elencoTitoliAutore[posizioneTitolo]=libro.getTitolo();
+                       posizioneTitolo++;
+                   }
+               }
+           }
+       }
+       return elencoTitoliAutore;
+   }
+   
+  public String toString()
+  {
+      String s="";
+      
+      if (getNumLibri()==0)
+          s="Nessun libro presente";
+      else
+      {
+        Libro libro;
+        for(int i=0;i<getNumRipiani();i++)
+        {
+            s=s+"Ripiano "+i+":\n";
+            for (int j=0;j<ripiani[i].getNumMaxVolumi();j++)
+            {
+                try
+                {
+                    if (getLibro(i, j)!=null)
+                    {
+                        libro=getLibro(i, j);
+                        s=s+j+" --> "+libro.toString()+"\n";
+                    } 
+                }
+                catch (EccezionePosizioneNonValida e1)
+                {
+                    
+                }                  
+            }
+        }     
+      }
+    return s;    
+  }
+  
+  public String elencoAlfabeticoLibri() throws EccezionePosizioneNonValida
+  {
+      String s="";
+      String elencoLibri[]=new String[getNumLibri()];
+      int c=0;
+      
+      if (getNumLibri()==0)
+          s="Nessun libro presente";
+      else
+      {
+        Libro libro;
+        for(int i=0;i<getNumRipiani();i++)
+        {
+            for (int j=0;j<ripiani[i].getNumMaxVolumi();j++)
+            {
+                if (getLibro(i, j)!=null)
+                {
+                    libro=getLibro(i, j);
+                    elencoLibri[c]=libro.getTitolo()+" --> ripiano:"+i+" posizione: "+j;
+                    c++;
+                }    
+            }
+        }
+        elencoLibri=Ordinatore.selectionSortCrescente(elencoLibri);
+        for(int i=0;i<elencoLibri.length;i++)
+            s+=elencoLibri[i]+"\n";
+      }
+    return s;    
+  }
+  
+  // restituisce un array di libri ordinati in base al prezzo
+  public Libro[] elencoLibriOrdinatiPrezzo() throws EccezionePosizioneNonValida
+  {
+      Libro[] elencoLibri=new Libro[getNumLibri()];
+      Libro libro;
+      int c=0;
+      
+      for(int i=0;i<NUM_RIPIANI;i++)
+      {
+          for(int j=0;j<ripiani[i].getNumMaxVolumi();j++)
+          {
+              libro=getLibro(i, j);
+              if (libro!=null)
+              {
+                  elencoLibri[c]=libro;
+                  c++;
+              }
+          }
+      }
+      
+      elencoLibri=Ordinatore.selectionSortLibriPrezzoCrescente(elencoLibri);
+      return elencoLibri;  
+  }
+  
+  
+  // restituisce un array di libri ordinati alfabeticamente per autore e in seguito per titolo
+  public Libro[] elencoLibriAlfabeticoAutoreTitolo() throws EccezionePosizioneNonValida
+  {
+      Libro[] elencoLibri=new Libro[getNumLibri()];
+      Libro libro;
+      int c=0;
+      
+      for(int i=0;i<NUM_RIPIANI;i++)
+      {
+          for(int j=0;j<ripiani[i].getNumMaxVolumi();j++)
+          {
+              libro=getLibro(i, j);
+              if (libro!=null)
+              {
+                  elencoLibri[c]=libro;
+                  c++;
+              }
+          }
+      }
+      
+      elencoLibri=Ordinatore.selectionSortAlfabeticoAutoreTitolo(elencoLibri);
+      return elencoLibri;  
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  /*
+  //ORDINAMENTO
+  public Libro[] elencoLibriOrdineAlfabeticoAutoreTitolo()
+  {
+      Libro[] elencoLibri=new Libro[getNumLibri()];
+      Libro libro;
+      int c=0;
+      
+      for(int i=0;i<NUM_RIPIANI;i++)
+      {
+          for(int j=0;j<ripiani[i].getNumMaxVolumi();j++)
+          {
+              libro=getLibro(i, j);
+              if (libro!=null)
+              {
+                  elencoLibri[c]=libro;
+                  c++;
+              }
+          }
+      }
+      
+      elencoLibri=Ordinatore.selectionSortLibriAutoreTitolo(elencoLibri);
+      return elencoLibri;  
+  }
+  
+  
+  
+  
+  
+  
+  public Libro[] elencoLibriOrdinatiPrezzo()
+  {
+      Libro[] elencoLibri=new Libro[getNumLibri()];
+      Libro libro;
+      int c=0;
+      
+      for (int i=0;i<NUM_RIPIANI;i++)
+      {
+          for (int j=0;j<ripiani[i].getNumMaxVolumi();j++)
+          {
+              libro=getLibro(i, j);
+              if (libro!=null)
+              {
+                  elencoLibri[c]=libro;
+                  c++;
+              }
+          }
+      }
+      
+      elencoLibri=Ordinatore.selectionSortPrezzoCrescente(elencoLibri);
+      return elencoLibri;
+      
+  }
+  
+  
+  public String elencoAlfabeticoLibri()
+  {
+      String s="";
+      String[] elencoLibri=new String[getNumLibri()];
+      int ripiano=0;
+      int posizione=0;
+      int c=0;
+      
+      if (getNumLibri()==0)
+          s="Nessun libro presente";
+      else
+      {
+        Libro libro;
+        for(int i=0;i<getNumRipiani();i++)
+        {
+            s=s+"Ripiano "+i+":\n";
+            for (int j=0;j<ripiani[i].getNumMaxVolumi();j++)
+            {
+                if (getLibro(i, j)!=null)
+                {   ripiano=i;
+                    posizione=j;
+                    libro=getLibro(i, j);
+                    elencoLibri[c]=libro.getTitolo()+" --> ripiano: "+ripiano+" posizione: "+posizione;
+                    c++;
+                }    
+            }
+        }
+        elencoLibri=Ordinatore.selectionSortCrescente(elencoLibri);
+        for (int i=0;i<elencoLibri.length;i++)
+            s=s+elencoLibri[i]+"\n";
+      }
+    return s;  
+  }
+  
+*/
+}
